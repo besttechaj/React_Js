e7e3b66c40f6fc97b880b9b115ee8d1a703887bd
Ajay Mishra <asap2656@gmail.com>
2024-04-15 23:50:32 +0530

    displaying the news using data.json

--- news_app/src/components/News.jsx
+++ news_app/src/components/News.jsx
@@ -3,7 +3,7 @@ import NewsItem from './NewsItem';
 import data from './sample.json';
 
 export default class News extends Component {
-  articles = [...data];
+  articles = [...data.articles];
 
   // calling the constructor to set the state
   constructor() {
@@ -19,32 +19,20 @@ export default class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h1>This is a news component</h1>
+        <h1>Top - Headlines</h1>
         <div className='row'>
-          <div className='col-md-4'>
-            <NewsItem
-              title='myTitle'
-              description='explanation...'
-              imageUrl='https://c.ndtvimg.com/2024-04/naqdpe5g_iran-drone-strikes-on-israel_625x300_14_April_24.jpeg?ver-20240316.08'
-              newsUrl='todo'
-            />
-          </div>
-          <div className='col-md-4'>
-            <NewsItem
-              title='myTitle'
-              description='explanation...'
-              imageUrl='https://c.ndtvimg.com/2024-04/naqdpe5g_iran-drone-strikes-on-israel_625x300_14_April_24.jpeg?ver-20240316.08'
-              newsUrl='todo'
-            />
-          </div>
-          <div className='col-md-4'>
-            <NewsItem
-              title='myTitle'
-              description='explanation...'
-              imageUrl='https://c.ndtvimg.com/2024-04/naqdpe5g_iran-drone-strikes-on-israel_625x300_14_April_24.jpeg?ver-20240316.08'
-              newsUrl='todo'
-            />
-          </div>
+          {this.state.articles.map((ele) => {
+            return (
+              <div className='col-md-4' key={ele.url}>
+                <NewsItem
+                  title={ele.title.slice(0, 40)}
+                  description={ele.description.slice(0, 60)}
+                  imageUrl={ele.urlToImage ? ele.urlToImage : 'no image'}
+                  newsUrl={ele.url}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
