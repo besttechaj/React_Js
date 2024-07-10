aef7de121a7960c9a01048aff3276e0be962196c
Ajay Mishra <asap2656@gmail.com>
2024-07-10 10:35:22 +0530

    added some styling

--- 11_reduxToolkit_todo/src/components/Todos.jsx
+++ 11_reduxToolkit_todo/src/components/Todos.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTodo } from '../features/todo/todoSlice';
+import './css/Todos.css';
 const Todos = () => {
   // fetching the data from the store
   const todos = useSelector((state) => state.todos);
 
   const dispatch = useDispatch();
   return (
-    <div>
+    <div className='outer'>
       <h1>list all todo</h1>
       <br />
       {todos.map((v, i) => (
         <React.Fragment key={i}>
-          <li key={v.id}>
-            {v.text}
-            <button onClick={() => dispatch(removeTodo(v.id))}>delete</button>
-          </li>
+          <div className='inner'>
+            <li key={v.id}>
+              {v.text}
+              <button onClick={() => dispatch(removeTodo(v.id))}>delete</button>
+            </li>
+          </div>
         </React.Fragment>
       ))}
     </div>
