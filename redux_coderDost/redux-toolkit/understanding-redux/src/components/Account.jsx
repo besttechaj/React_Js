899b098ac7c50ed62fe56c49499811a0ae049737
Ajay Mishra <asap2656@gmail.com>
2024-09-01 00:11:14 +0530

    update

--- redux_coderDost/redux-toolkit/understanding-redux/src/components/Account.jsx
+++ redux_coderDost/redux-toolkit/understanding-redux/src/components/Account.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement, incrementByAmount } from '../actions';
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  getUserAccount,
+} from '../actions';
 
 const Account = () => {
   const [value, setValue] = useState(0);
@@ -25,9 +30,13 @@ const Account = () => {
         <button onClick={() => dispatch(incrementByAmount(value))}>
           Increment By {value} +
         </button>
+{/* passing specific user's id in getUserAccount */}
+        <button onClick={() => dispatch(getUserAccount(1))}>
+          Initialize account
+        </button>
       </div>
     </div>
   );
 };
-
+2 45555555555555
 export default Account;
